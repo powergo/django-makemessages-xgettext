3db5c62d116399a0b26e8c68d3e4274d5a6bd17b
Barthelemy Dagenais <barthelemy@infobart.com>
2015-12-23 15:11:07 -0500

    Support for django 1.9 and beyond!

--- django_makemessages_xgettext/management/commands/makemessagesxgettext.py
+++ django_makemessages_xgettext/management/commands/makemessagesxgettext.py
@@ -1,9 +1,8 @@
 import django
 
-if django.get_version().startswith("1.8"):
-    from django_makemessages_xgettext import django18_makemessagesxgettext
-    Command = django18_makemessagesxgettext.Command
-
-else:
+if django.get_version().startswith("1.7"):
     from django_makemessages_xgettext import django17_makemessagesxgettext
     Command = django17_makemessagesxgettext.Command
+else:
+    from django_makemessages_xgettext import django18_makemessagesxgettext
+    Command = django18_makemessagesxgettext.Command
